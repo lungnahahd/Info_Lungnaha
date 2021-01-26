0d6bad1d32067005a46b4d4dd39aab9043f0aa52
lungnahahd <jyoungjk33@gmail.com>
2021-01-26 22:03:25 +0900

    Fix_views.py(메뉴 구현)

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -5,4 +5,7 @@ import main.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main.views.home),
+    path('one.html',main.views.one),
+    path('two.html',main.views.two),
+    path('three.html',main.views.three),
 ]
--- Server/main/views.py
+++ Server/main/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home.html')
 
-# Create your views here.
+def one(request):
+    return render(request,'one.html')
+
+def two(request):
+    return render(request,'two.html')
+
+def three(request):
+    return render(request,'three.html')
