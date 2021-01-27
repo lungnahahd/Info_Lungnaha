b0232e7e3bdc4772cccce4e4c0844cb87bf158a6
lungnahahd <jyoungjk33@gmail.com>
2021-01-28 00:24:20 +0900

    Fix_settings.py(Mysql 연동)

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lungnaha_info',
+        'USER' : 'root',
+        'PASSWORD' : 'khd10810',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
