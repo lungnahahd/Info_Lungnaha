1d91fe4e91a6865c7c79a3ddf75b9019935460e1
lungnahahd <jyoungjk33@gmail.com>
2021-01-28 00:45:31 +0900

    Add_serializers.py(직렬화 생성)

---
+++ Server/main/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import infolist
+
+class listserializers(serializers.ListSerializer):
+    class Meta:
+        model = infolist
+        fields = ('id','title','information','now')
