1cbffb8c549eb39aa1a28b59e88d0a1983162406
lungnahahd <jyoungjk33@gmail.com>
2021-01-28 23:59:01 +0900

    Fix_views.py(viewset 설정)

--- Server/main/views.py
+++ Server/main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .serializers import listserializers
+from rest_framework import viewsets
+from .models import infolist
 
 def home(request):
     return render(request,'home.html')
@@ -11,3 +14,8 @@ def two(request):
 
 def three(request):
     return render(request,'three.html')
+
+
+class infoviewset(viewsets.infoviewset):
+    queryset = infolist.objectes.all()
+    serializer_class = listserializers
