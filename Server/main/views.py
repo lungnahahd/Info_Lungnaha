bafb6509abc16f9620c2358f5d6779645c6cc48d
lungnahahd <jyoungjk33@gmail.com>
2021-02-08 01:23:46 +0900

    Fix_infodetail.html(제목, 내용 구분 구현)

--- Server/main/views.py
+++ Server/main/views.py
@@ -3,6 +3,9 @@ from .serializers import listserializers
 from rest_framework import viewsets
 from .models import infolist
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.template.defaultfilters import register
 
 def home(request):
     return render(request,'home.html')
@@ -28,14 +31,28 @@ def saveinfo(request):
       post.title = request.POST['title']
       post.information = request.POST['information']
       post.save()
-    return redirect('writeinfo.html')  
+    return redirect('http://localhost:8000/')  
 
 def showresult(request):
     resultlist = infolist.objects.all()
     return render(request,'result.html', {'resultlist' : resultlist})
 
+@method_decorator(csrf_exempt,name='dispatch')
 def showdetail(request):
-    return render(request,'infodetail.html')
+    if request.method == "GET":
+        return render(request,'infodetail.html')
+    elif request.method == "POST":
+        code = request.POST.get('id')
+        recode = code[:-1]
+        detail = infolist.objects.get(id = recode)
+        # detail = infolist.objects.get(information = request)
+        return render(request,'infodetail.html',{'detail':detail})        
+        
+
+        # post = infolist()
+        # post.title = request.POST['title']
+        # post.information = request.POST['information']
+        # return redirect('infodetail.html')
 
 
 class infoviewset(viewsets.ModelViewSet):
