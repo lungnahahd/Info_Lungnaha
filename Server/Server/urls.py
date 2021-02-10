969d3f9437897fd757d3e8d10ab16d6310ba426c
lungnahahd <jyoungjk33@gmail.com>
2021-02-10 23:25:41 +0900

    Fix_views.py(로그인 구현 완료)

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls import url,include
 from rest_framework import routers
 from main.views import infoviewset
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 router = routers.DefaultRouter()
 router.register('main',infoviewset)
 
@@ -18,6 +21,8 @@ urlpatterns = [
     path('saveinfo',main.views.saveinfo,name='saveinfo'),
     path('result.html',main.views.showresult),
     path('infodetail.html',main.views.showdetail),
-    path('login.html',main.views.login),
-    path('signup.html',main.views.signup),
-]
+    path('login.html/',main.views.login,name='login'),
+    #path('login/',main.views.login,name = 'login'),
+    #path('signup/',main.views.signup,name='signup'),
+    path('signup.html/',main.views.signup,name='signup'),
+] 
--- Server/main/views.py
+++ Server/main/views.py
@@ -52,17 +52,26 @@ def showdetail(request):
         
 
 def login(request):
-    return render(request,'login.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('http://localhost:8000/')
+        else :
+            return render(request, 'login.html', {'error' : '아이디 또는 비밀번호가 옳지 않습니다. ㅜㅜ'})
+    else:
+        return render(request,'login.html')
 
 def signup(request):
     if request.method =="POST":
         if request.POST["password1"] == request.POST["password2"]:
             user = User.objects.create_user(
-                username = request.POST["id"], password = request.POST["password1"])
+                username = request.POST["username"], password = request.POST["password1"])
             auth.login(request, user)
             return redirect('http://localhost:8000/')
-        return render(request,'signup.html')
-    return render(request, 'signup.html')
+    return render(request,'signup.html')
 
 
 class infoviewset(viewsets.ModelViewSet):
